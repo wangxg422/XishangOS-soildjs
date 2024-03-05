8ca9764a0986b7e4470227bcd4e2644f381e0b43
wangxg <wangxg422@outlook.com>
2024-03-05 23:18:04 +0800

    时间居中

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import type { Component } from "solid-js"
 import { Router } from "@solidjs/router";
 import routes from "./router";
 import "./index.css";
-import "./input.css";
-
 
 const App: Component = () => {
   return <Router>{routes}</Router>
--- src/views/desktop/index.tsx
+++ src/views/desktop/index.tsx
@@ -1,12 +1,14 @@
 import { onMount, onCleanup } from "solid-js";
 import dayjs from "dayjs";
 import bgImage from "../../assets/images/bg.jpg";
+import "./index.scss";
+import { getWeekInChinese } from "../../utils/datetime";
 
 export default function Desktop() {
   let dateTime = $signal({
     time: "",
     date: "",
-    week: "",
+    week: 0,
   });
 
   let timerId: NodeJS.Timeout;
@@ -22,19 +24,18 @@ export default function Desktop() {
     dateTime = {
       time: now.format("HH:mm:ss"),
       date: now.format("YYYY 年 MM 月 DD 日"),
-      week: now.day() + "",
+      week: now.day(),
     };
   };
 
   return (
     <>
     {/* bg-[url(${bgImage})] */}
-      <div class={`desktop h-600 w-full bg-cover`} style={{'background-image': `url(${bgImage})`}}>
-        <div class="datetime">
-          <div class="time">{dateTime.time}</div>
-          <div class="date">
-            {dateTime.date}
-            {dateTime.week}
+      <div class={`desktop h-screen w-screen bg-cover`} style={{'background-image': `url(${bgImage})`}}>
+        <div class="datetime text-white">
+          <div class="text-6xl font-medium">{dateTime.time}</div>
+          <div class="text-xl font-bold mt-2">
+            <span>{dateTime.date}</span><span class="ml-2">{getWeekInChinese(dateTime.week)}</span>  
           </div>
         </div>
       </div>
