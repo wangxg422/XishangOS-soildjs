7d34bf633edbe516dc56a1fbf01b40d8d3e27678
wangxg <wangxg422@outlook.com>
2024-03-10 15:33:27 +0800

    整理vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,88 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import solidLabels from 'babel-plugin-solid-labels';
-import path from 'node:path';
+import { ConfigEnv, UserConfig, defineConfig, loadEnv } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import solidLabels from "babel-plugin-solid-labels";
+import path from "node:path";
+import { wrapperEnv } from "./build/env";
+import { createProxy } from "./build/proxy";
+import dayjs from "dayjs";
+import pkg from "./package.json";
 // import devtools from 'solid-devtools/vite';
 
-export default defineConfig({
-  base: "/",
-  plugins: [
-    /* 
+const { dependencies, devDependencies, name, version } = pkg;
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: dayjs().format("YYYY-MM-DD HH:mm:ss")
+};
+
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+  const viteEnv = wrapperEnv(env);
+  return {
+    base: viteEnv.VITE_PUBLIC_PATH || "/",
+    root,
+    plugins: [
+      /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
-    // devtools(),
-    solidPlugin({
-      babel: {
-        plugins: [
-          [solidLabels, { dev: process.env.NODE_ENV !== 'production' }]
-        ],
-      }}),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src')
+      // devtools(),
+      solidPlugin({
+        babel: {
+          plugins: [
+            [solidLabels, { dev: process.env.NODE_ENV !== "production" }],
+          ],
+        },
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: path.resolve(__dirname, "src"),
+        },
+      ],
+    },
+    define: {
+      __APP_INFO__: JSON.stringify(__APP_INFO__)
+    },
+    server: {
+      host: "0.0.0.0",
+      port: viteEnv.VITE_PORT,
+      open: viteEnv.VITE_OPEN,
+      cors: true,
+      // Load proxy configuration from .env.development
+      proxy: createProxy(viteEnv.VITE_PROXY),
+    },
+    esbuild: {
+      pure: viteEnv.VITE_DROP_CONSOLE ? ["console.log", "debugger"] : []
+    },
+    build: {
+      target: "esnext",
+      assetsDir: "assets",
+      outDir: "dist",
+      minify: "esbuild",
+      // esbuild 打包更快，但是不能去除 console.log，terser打包慢，但能去除 console.log
+      // minify: "terser",
+      // terserOptions: {
+      // 	compress: {
+      // 		drop_console: viteEnv.VITE_DROP_CONSOLE,
+      // 		drop_debugger: true
+      // 	}
+      // },
+      sourcemap: false,
+      // 禁用 gzip 压缩大小报告，可略微减少打包时间
+      reportCompressedSize: false,
+      // 规定触发警告的 chunk 大小
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          // Static resource classification and packaging
+          chunkFileNames: "assets/js/[name]-[hash].js",
+          entryFileNames: "assets/js/[name]-[hash].js",
+          assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+        }
       }
-    ]
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-    minify: "esbuild",
-    assetsDir: "assets",
-  },
+    },
+  };
 });
