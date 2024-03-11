be5a966c2f6cbad55e20c7664e68888062de8bf1
wangxg <wangxg422@outlook.com>
2024-03-11 22:37:09 +0800

    app的背景从scss文件中提出来

--- src/views/desktop/components/appInstance/index.tsx
+++ src/views/desktop/components/appInstance/index.tsx
@@ -13,10 +13,18 @@ export default function AppInstance(props: any) {
   const appInstance: App.Instance = props.appInstanceInfo;
   
   const icon = appInstance.instanceIcon ? appInstance.instanceIcon : defaultAppImg;
+
+  const iconBg = {
+      "background": `rgba(255, 255, 255, 0.90)`,
+      "-webkit-backdrop-filter": `blur(5px)`,
+      "backdrop-filter": `blur(5px)`,
+      "transition": `0.5s ease`
+  }
+
   return (
     <>
-      <div class="app-item cursor-pointer hvr-pulse-shrink flex flex-col items-center" onclick={clickApp}>
-        <div class={`${appBg} w-16 h-16 rounded-md flex items-center justify-center`}>
+      <div class="cursor-pointer hvr-pulse-shrink flex flex-col items-center" onclick={clickApp}>
+        <div class={`w-16 h-16 rounded-md flex items-center justify-center`} style={iconBg}>
           <div class="w-12 h-12">
             <img src={icon} alt="app" />
           </div>
