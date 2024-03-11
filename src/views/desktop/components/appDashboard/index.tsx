f78b594a7836f9695ce5156f2f96b79e8e3673a0
wangxg <wangxg422@outlook.com>
2024-03-11 19:28:45 +0800

    fix: 修改appBoard中的appInfo->appInstance

--- src/views/desktop/components/appDashboard/index.tsx
+++ src/views/desktop/components/appDashboard/index.tsx
@@ -20,7 +20,7 @@ export default function AppDashboard() {
         <For each={appList}>
           {(app) => (
             <div>
-              <AppItem appInfo={app} />
+              <AppItem appInstanceInfo={app} />
             </div>
           )}
         </For>
