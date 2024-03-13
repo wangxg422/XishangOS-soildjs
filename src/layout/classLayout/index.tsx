ccb6544bef29ddbb1a5cbf49bf19d4e5af21de24
wangxg <35413234+wangxg422@users.noreply.github.com>
2024-03-13 17:02:47 +0800

    经典布局

--- src/layout/classLayout/index.tsx
+++ src/layout/classLayout/index.tsx
@@ -1,12 +1,18 @@
 import { ParentProps, type Component } from "solid-js";
+import "./index.scss";
 
 export interface ClassicLayoutProps extends ParentProps {}
 
 const ClassLayout: Component<ClassicLayoutProps> = (props) => {
+
   return (
     <>
-    Layout
-      {props.children}
+      <div class="classic-layout w-screen h-screen">
+        <div class="header"></div>
+        <div class="sidebar"></div>
+        <div class="main">{props.children}</div>
+        <div class="footer"></div>
+      </div>
     </>
   );
 };
