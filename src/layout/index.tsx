f882b55449d6e6df3554e40a2aca771a7fd6c41b
wangxg <wangxg422@outlook.com>
2024-03-15 22:31:06 +0800

    又layout控制显示的布局样式

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,14 +1,26 @@
 import type { Component, ParentProps } from "solid-js";
 import ClassLayout from "./classLayout";
 import HorizontalLayout from "./horizontalLayout";
+import VerticalLayout from "./verticalLayout";
 
 export interface LayoutProps extends ParentProps {}
 
 const Layout: Component<LayoutProps> = (props) => {
+  const layout: string = $signal("vertical");
   return (
     <>
-      {/* <ClassLayout>{props.children}</ClassLayout> */}
       <HorizontalLayout>{props.children}</HorizontalLayout>
+      <Switch fallback={<ClassLayout>{props.children}</ClassLayout>}>
+        <Match when={layout === "classic"}>
+          <ClassLayout>{props.children}</ClassLayout>
+        </Match>
+        <Match when={layout === "vertical"}>
+          <VerticalLayout>{props.children}</VerticalLayout>
+        </Match>
+        <Match when={layout === "horizontal"}>
+          <HorizontalLayout>{props.children}</HorizontalLayout>
+        </Match>
+      </Switch>
     </>
   );
 };
---
+++ src/layout/verticalLayout/index.tsx
@@ -0,0 +1,31 @@
+import { ParentProps, type Component } from "solid-js";
+import "./index.scss";
+import HeaderLeft from "../components/headerLeft";
+import Footer from "../components/footer";
+import HeaderRight from "../components/headerRight";
+import Sidebar from "../components/sidebar";
+import HeaderMid from "../components/headerMid";
+import Main from "../components/main";
+
+export interface VerticalLayoutProps extends ParentProps {}
+
+const VerticalLayout: Component<VerticalLayoutProps> = (props) => {
+  return (
+    <>
+      <div class="classic-layout w-screen h-screen">
+        <div class="header border-b">
+          <div class="h-full">
+            <HeaderLeft />
+          </div>
+          <div class=""><HeaderMid /></div>
+          <div><HeaderRight /></div>
+        </div>
+        <div class="sidebar"><Sidebar /></div>
+        <div class="main"><Main>{props.children}</Main></div>
+        <div class="footer"><Footer /></div>
+      </div>
+    </>
+  );
+};
+
+export default VerticalLayout;
