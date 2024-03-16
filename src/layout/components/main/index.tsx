f4b2efc1544680edc7040734535702f3b6d15343
wangxg <wangxg422@outlook.com>
2024-03-16 21:19:34 +0800

    独立出tabbar组件

--- src/layout/components/main/index.tsx
+++ src/layout/components/main/index.tsx
@@ -1,29 +1,13 @@
-import { A } from "@solidjs/router";
 import { ParentProps, type Component } from "solid-js";
-import { AiFillCloseCircle, AiOutlineClose } from 'solid-icons/ai'
-import { useSysMenuTabBarStore } from "@/store/system/menu/tabBarStore";
+import TabBar from "../tabBar";
 
 export interface MainProps extends ParentProps {}
 
 const Main: Component<MainProps> = (props) => {
-  const tabBarList = useSysMenuTabBarStore(state => state.tabBar);
-
   return (
     <>
-      <div class="w-full h-8 flex justify-start items-center bg-white">
-        <For each={tabBarList}>
-          {(tab) => {
-            return (
-              <div class="h-full mx-1 flex justify-start items-center cursor-pointer hover:font-bold">
-                <div></div>
-                <div>
-                  <A href={tab.path}>{tab.title}</A>
-                </div>
-                <div><AiOutlineClose /></div>
-              </div>
-            );
-          }}
-        </For>
+      <div>
+        <TabBar />
       </div>
       <div class="px-1 py-1">{props.children}</div>
     </>
---
+++ src/layout/components/tabBar/index.tsx
@@ -0,0 +1,32 @@
+import { A } from "@solidjs/router";
+import { ParentProps, type Component } from "solid-js";
+import { AiFillCloseCircle, AiOutlineClose } from 'solid-icons/ai'
+import { useSysMenuTabBarStore } from "@/store/system/menu/tabBarStore";
+
+export interface TabBarProps extends ParentProps {}
+
+const TabBar: Component<TabBarProps> = (props) => {
+  const tabBarList = useSysMenuTabBarStore(state => state.tabBar);
+
+  return (
+    <>
+      <div class="w-full h-8 flex justify-start items-center bg-white">
+        <For each={tabBarList}>
+          {(tab) => {
+            return (
+              <div class="h-full mx-1 flex justify-start items-center cursor-pointer hover:font-bold">
+                <div></div>
+                <div>
+                  <A href={tab.path}>{tab.title}</A>
+                </div>
+                <div><AiOutlineClose /></div>
+              </div>
+            );
+          }}
+        </For>
+      </div>
+    </>
+  );
+};
+
+export default TabBar;
