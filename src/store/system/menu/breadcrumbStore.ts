3e8e38f748e554b7b414e3f1a5b60fa2934e3a1f
wangxg <wangxg422@outlook.com>
2024-03-17 20:50:26 +0800

    面包屑再添加持久化

--- src/store/system/menu/breadcrumbStore.ts
+++ src/store/system/menu/breadcrumbStore.ts
@@ -1,6 +1,7 @@
 import { createWithStore } from "solid-zustand";
 import { SysMenu } from "@/interface/system/menu";
 import { useSysAllMenuBreadcrumb } from "./allMenuBreadcrumbStore";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export interface SysMenuBreadcrumbStore {
   breadcrumb: SysMenu.IBreadcrumb[];
@@ -11,14 +12,21 @@ const allMenuBreadcrumb = useSysAllMenuBreadcrumb(
   (state) => state.allMenuBreadcrumb
 );
 
-export const useSysMenuBreadcrumbStore =
-  createWithStore<SysMenuBreadcrumbStore>((set) => ({
-    breadcrumb: [],
-    setBreadcrumb: (menuName: string) =>
-      set(() => {
-        console.log('all:', allMenuBreadcrumb)
-        console.log('name:', menuName)
-        console.log('bread:', allMenuBreadcrumb[menuName])
-        return { breadcrumb: allMenuBreadcrumb[menuName] };
-      }),
-  }));
+export const useSysMenuBreadcrumbStore = createWithStore(
+  persist<SysMenuBreadcrumbStore>(
+    (set) => ({
+      breadcrumb: [],
+      setBreadcrumb: (menuName: string) =>
+        set(() => {
+          console.log("all:", allMenuBreadcrumb);
+          console.log("name:", menuName);
+          console.log("bread:", allMenuBreadcrumb[menuName]);
+          return { breadcrumb: allMenuBreadcrumb[menuName] };
+        }),
+    }),
+    {
+      name: "allMenuBreadcrumb",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
