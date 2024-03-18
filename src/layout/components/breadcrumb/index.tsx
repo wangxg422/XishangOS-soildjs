7a41fbe2e32567d264d1722a4d6659c52a852505
wangxg <35413234+wangxg422@users.noreply.github.com>
2024-03-18 14:19:49 +0800

    独立出面包屑组件

---
+++ src/layout/components/breadcrumb/index.tsx
@@ -0,0 +1,50 @@
+import { useSysMenuBreadcrumbStore } from "@/store/system/menu/breadcrumbStore";
+import { ParentProps, type Component } from "solid-js";
+import { RiSystemApps2Fill } from "solid-icons/ri";
+import { AiOutlineRight } from "solid-icons/ai";
+import { MenuTypeEnum } from "@/utils/enums/menu";
+import { A } from "@solidjs/router";
+
+export interface BreadcrumbProps extends ParentProps {}
+
+const Breadcrumb: Component<BreadcrumbProps> = (props) => {
+  const breadcrumbList = useSysMenuBreadcrumbStore((state) => state.breadcrumb);
+
+  return (
+    <>
+      <div class="w-full h-full flex justify-start items-center">
+        <For each={breadcrumbList}>
+          {(b, i) => {
+            const icon = <RiSystemApps2Fill />;
+            const separator = <AiOutlineRight />;
+
+            let content;
+            if (b.type === MenuTypeEnum.DIR) {
+              content = (
+                <div class="flex justify-center items-center">
+                  <div class="mx-1">{i() !== 0 && separator}</div>
+                  <div>{icon}</div>
+                  <div class="mx-1">{b.title}</div>
+                </div>
+              );
+            } else if (b.type === MenuTypeEnum.MENU) {
+              content = (
+                <div class="flex justify-center items-center">
+                  <div class="mx-1">{i() !== 0 && separator}</div>
+                  <div> {icon}</div>
+                  <div class="mx-1 hover:font-bold">
+                    <A href={b.path}>{b.title}</A>
+                  </div>
+                </div>
+              );
+            }
+
+            return <div>{content}</div>;
+          }}
+        </For>
+      </div>
+    </>
+  );
+};
+
+export default Breadcrumb;
--- src/layout/components/headerMid/index.tsx
+++ src/layout/components/headerMid/index.tsx
@@ -1,9 +1,6 @@
-import { MenuTypeEnum } from "@/utils/enums/menu";
-import { A } from "@solidjs/router";
 import { ParentProps, type Component } from "solid-js";
-import { RiSystemApps2Fill } from "solid-icons/ri";
-import { AiOutlineRight } from "solid-icons/ai";
 import { useSysMenuBreadcrumbStore } from "@/store/system/menu/breadcrumbStore";
+import Breadcrumb from "../breadcrumb";
 
 export interface HeaderMidProps extends ParentProps {}
 
@@ -12,36 +9,8 @@ const HeaderMid: Component<HeaderMidProps> = () => {
 
   return (
     <>
-      <div class="h-full w-full flex justify-start items-center">
-        <For each={breadcrumbList}>
-          {(b, i) => {
-            const icon = <RiSystemApps2Fill />;
-            const separator = <AiOutlineRight />;
-
-            let content;
-            if (b.type === MenuTypeEnum.DIR) {
-              content = (
-                <div class="flex justify-center items-center">
-                  <div class="mx-1">{i() !== 0 && separator}</div>
-                  <div>{icon}</div>
-                  <div class="mx-1">{b.title}</div>
-                </div>
-              );
-            } else if (b.type === MenuTypeEnum.MENU) {
-              content = (
-                <div class="flex justify-center items-center">
-                  <div class="mx-1">{i() !== 0 && separator}</div>
-                  <div> {icon}</div>
-                  <div class="mx-1 hover:font-bold">
-                    <A href={b.path}>{b.title}</A>
-                  </div>
-                </div>
-              );
-            }
-
-            return <div>{content}</div>;
-          }}
-        </For>
+      <div class="h-full w-full">
+        <Breadcrumb />
       </div>
     </>
   );
